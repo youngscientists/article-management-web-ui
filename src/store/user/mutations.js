957b7f6dcf28abd57c41ee80506328ea6ae0b1e3
Stewart McGown <stewartcallummcgown@gmail.com>
2019-05-02 01:17:50 +0100

    [auth] Fixes  logout persistence

--- src/store/user/mutations.js
+++ src/store/user/mutations.js
@@ -11,6 +11,9 @@ import { SET } from "./mutation-types"
 /* eslint-disable no-param-reassign */
 export default {
 	[SET](state, user) {
-		Object.assign(state, user)
+		if (user == null) {
+			Object.keys(state).forEach(k => state[k] = null)
+		} else {
+		Object.assign(state, user)}
 	}
 }
